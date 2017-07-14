9c251a24eebf718da99bbd8f53107b3571d0aeb9
smartGoPc <smartgo666@gmail.com>
2017-07-14 09:53:40 +0800

    Initial commit

---
+++ src/com/smartgo/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.smartgo.demo;
+
+/**
+ * Created by WangPengcheng on 2017/7/13.
+ */
+public class HelloWorld {
+    public static  void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
